3c4838b4a3045148002392ec5c2544ba2e78ee2a
kuwakeeen <64997597+kuwakeeen@users.noreply.github.com>
2020-05-08 10:54:01 +0900

    Add files via upload

---
+++ test2.cpp
@@ -0,0 +1,16 @@
+/*
+ * test2.cpp
+ *
+ *  Created on: 2020/05/08
+ *      Author: kuwa
+ */
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "hello, world" << std::endl;  // 文字列を出力
+    return 0;                                  // 実行環境に 0 を返す
+}
+
+
