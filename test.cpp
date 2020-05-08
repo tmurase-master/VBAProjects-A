91b1b4f23f3582ef6174edf575d8108a16a331ad
kuwakeeen <fts175r@gmail.com>
2020-05-08 16:49:32 +0900

    アップロードテスト！

---
+++ test.cpp
@@ -0,0 +1,14 @@
+/*
+ * test1.cpp
+ *
+ *  Created on: 2020/05/08
+ *      Author: kuwa
+ */
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "hello, world" << std::endl;  // 文字列を出力
+    return 0;                                  // 実行環境に 0 を返す
+}
