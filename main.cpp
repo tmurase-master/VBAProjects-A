6730600fdd361f1bb0c31c230f919c1ae3e447be
romgit-training <rom.masato@gmail.com>
2020-05-11 17:08:15 +0900

    更新

--- main.cpp
+++ main.cpp
@@ -1,8 +1,39 @@
-#include <stdio.h>
+#include<stdio.h>
+#include<time.h>
+#include<sys/types.h>
+#include<utime.h>
+#include<sys/stat.h>
+
+class timestamp_manager{
+
+ private:
+  struct utimbuf current_time;
+  
+
+ public:
+  void set_current_time();
+  void time_update(char *filepath);
+
+};
 
 int main(void){
 
-    printf("プッシュできてるのか？\n"); 
-    
+    timestamp_manager test;
+
+    test.set_current_time();
+
     return 0;
+}
+
+void timestamp_manager::set_current_time()
+{
+    current_time.actime=time(NULL);
+    current_time.modtime=current_time.actime;
+
+    //printf("%ld|n", current_time.modtime);
+}
+
+void timestamp_manager::time_update(char *filepath)
+{
+    utime(filepath, &this->current_time);
 }
