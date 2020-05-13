aa980646c82226b526a957aa831e1c97a59a7d7c
romgit-training <rom.masato@gmail.com>
2020-05-13 22:58:06 +0900

    まだ動作確認はしていませんが。。。

---
+++ timestamp.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#include<time.h>
+#include<sys/types.h>
+#include<utime.h>
+#include<sys/stat.h>
+
+
+int ts_update_currenttime(char *filepath);
+int ts_update_specifiedtime(char *filepath, struct utimbuf *settime);
+int ts_update_specifiedYMD(char *filepath, int year,int month, int day);
+
+int ts_update_currenttime(char *filepath){
+    struct utimbuf ct;
+
+    ct.actime = time(NULL);
+    ct.modtime = ct.actime;
+    utime(*filepath, &ct);
+
+    return 0;
+}
+
+int ts_update_specifiedtime(char *filepath, struct utimbuf *settime){
+
+    if(settime->actime != NULL){
+        utime(*filepath, &settime);
+        return 0;
+    }
+    else return 1;
+}
+
+int ts_update_specifiedYMD(char *filepath, int year,int month, int day){
+    
+}
+
+
+int main(void){
+
+    printf("test");
+
+}
