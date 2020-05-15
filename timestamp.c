98febd17241ce247d57afe26248d9ec5e6888131
romgit-training <rom.masato@gmail.com>
2020-05-15 21:46:46 +0900

    更新

--- timestamp.c
+++ timestamp.c
@@ -14,9 +14,9 @@ int ts_update_currenttime(char *filepath){
 
     ct.actime = time(NULL);
     ct.modtime = ct.actime;
-    utime(*filepath, &ct);
+    int r = utime(filepath, &ct);
 
-    return 0;
+    return r;
 }
 
 int ts_update_specifiedtime(char *filepath, struct utimbuf *settime){
@@ -30,17 +30,23 @@ int ts_update_specifiedtime(char *filepath, struct utimbuf *settime){
 
 int ts_update_specifiedYMD(char *filepath, int year,int month, int day, int hour, int min, int sec){
     
-    struct tm YMD = {sec, min, hour, day, month, (year-1900)};
+    struct tm YMD = {sec, min, hour, day, (month-1), (year-1900)};
     struct utimbuf ct;
 
     ct.actime = mktime(&YMD);
     ct.modtime = ct.actime;
-    utime(*filepath, &ct);
+    utime(filepath, &ct);
+    //utime("test.rtf", &ct);
+
+
 }
 
 
 int main(void){
 
-    printf("test");
+    char file[9] = "test.rtf"; 
+    ts_update_specifiedYMD(file, 1986,8,1,12,10,5);
+    //ts_update_currenttime("test.rtf");
+    //printf("test");
 
 }
