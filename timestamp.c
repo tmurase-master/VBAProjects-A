58967ca271f3f59b7ea59ce023b93225c794f126
kycoo1800 <kycoo1800@gmail.com>
2020-05-16 14:48:40 +0900

    任意のファイルパスを選択できるようにしました

--- timestamp.c
+++ timestamp.c
@@ -4,10 +4,20 @@
 #include<utime.h>
 #include<sys/stat.h>
 
+//清水追加分
+#include<stdio.h>
+#include<string.h>
+#include<unistd.h>
+#define PATHNAME_SIZE 512
+//
+
 
 int ts_update_currenttime(char *filepath);
 int ts_update_specifiedtime(char *filepath, struct utimbuf *settime);
 int ts_update_specifiedYMD(char *filepath, int year,int month, int day, int hour, int min, int sec);
+//清水追加分
+void get_filepath(char *filepath);
+//
 
 int ts_update_currenttime(char *filepath){
     struct utimbuf ct;
@@ -48,8 +58,38 @@ int ts_update_specifiedYMD(char *filepath, int year,int month, int day, int hour
     }
 }
 
+//清水追加分
+void get_filepath(char *filepath){
+
+   // 変数定義
+    char pathname[PATHNAME_SIZE];  // ファイルパス
+    char cdp[256]; //任意のファイルパス入力用
+
+    // 変数初期化
+    memset(pathname, '\0', PATHNAME_SIZE); 
+
+    // 任意のファイルパスの入力
+    printf("Input Current Directory > ");
+	scanf("%[^\n]%*c", filepath);
+
+    // カレントディレクトリ取得
+    getcwd(pathname, PATHNAME_SIZE);
+    fprintf(stdout,"Before FilePath:%s\n", pathname);
+
+    // カレントディレクトリ変更  
+    chdir(filepath); // チェンジディレクトリ
+    getcwd(pathname, PATHNAME_SIZE);
+    fprintf(stdout,"After FilePath:%s\n", pathname);
+}
+//
+
 
 int main(void){
+
+    //清水追加分:任意のファイルパスを取得する関数の呼び出し
+    char filepath[256];
+    get_filepath(filepath);
+    //
 
     char file[9] = "test.rtf"; 
     ts_update_specifiedYMD(file, 1986,8,1,12,10,5);
